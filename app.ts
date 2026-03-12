9a2b93ed521f710144a478a357b07ecb8cc621a0
shruthid-27 <shruthidhanasekar27@gmail.com>
2026-03-12 21:35:08 +0530

    Add files via upload

---
+++ app.ts
@@ -0,0 +1,45 @@
+import { CommonModule, NgClass } from '@angular/common';
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { RouterOutlet } from '@angular/router';
+
+export interface TodoItem {
+  id: number;
+  task: string;
+  completed: boolean
+}
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet, FormsModule, CommonModule, NgClass],
+  templateUrl: './app.html',
+  styleUrl: './app.css'
+})
+export class AppComponent {
+  todoList: TodoItem[] = [];
+  newTask: string = ''
+
+  addTask(): void {
+
+    if (this.newTask.trim() !== '') {
+      const newTodoItem: TodoItem = {
+        id: Date.now(),
+        task: this.newTask,
+        completed: false
+      }
+
+      this.todoList.push(newTodoItem)
+      this.newTask = ''
+    }
+  }
+  toggleCompleted(index: number): void {
+    console.log(index);
+    this.todoList[index].completed = !this.todoList[index].completed
+    console.log(this.todoList);
+  }
+  deleteTask(id: number): void {
+    this.todoList = this.todoList.filter(item => item.id !== id)
+    console.log(this.todoList);
+
+  }
+}
